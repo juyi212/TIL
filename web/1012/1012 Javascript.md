de877b0ccc47556d32580ff6c81842f85228e72a
juyi <dea8307@naver.com>
2020-10-12 21:41:35 +0900

    1012 study

---
+++ web/1012/1012 Javascript.md
@@ -0,0 +1,117 @@
+# 1012 Javascript
+
+>브라우저 화면을 동적으로 만들기 위해서 쓴다! (브라우저 조작!)
+>
+>HTML + CSS (정적) + Javascript (동적)
+
+
+
+### History of Browser & Javascript
+
+- 1994년 네스케이프사의 브라우저가 표준 
+- 정적인 html을 동적으로 표현하기 위한 언어 도입을 결정
+- 1995년 브랜던 아이크 주도로 개발된 'Mocha'를 자체 브라우저에 도입
+- 이후 livescript라는 이름을 거쳐 지금의 javascript로 변경 
+
+- MS의 폭발적 성장, IE3에서 자체적인 JScirpt 를 지원, 호환성 문제로 크로스 브라우징 등의 이슈 발생
+- 계속되는 파편화를 방지하고 모든 브라우저에서 동일하기 동작하기 위한 표준의 필요성 제기
+- Chrome 등장 이후 여러 과정을 거쳐 자바스크립트의 고질적인 문제를 많이 해결한 ES6 등장!
+
+
+
+1. 브라우저 전쟁
+2. 파편화 & 표준화의 투쟁
+   - 브라우저 전쟁의 여파로  Cross browsing lssue
+   - 표준화 통합을 위한 노력 jQuery
+   - Vanilla JavaScript ! 사용
+
+
+
+## DOM 의 개념
+
+> Document Object Model (문서 객체 모델)
+>
+> 웹 페이지 문서 구조를 표현함으로써 스크립트 및 프로그래밍 언어와 페이지를 연결한다. ( JS- HTML )
+>
+> DOM은 문서를 논리 트리로 표현 , 각 노드는 객체를 갖는다
+
+- HTML, XML등과 같은  문서를 다루기 위한 언어 독립적인 문서 모델 인터페이스
+
+- 문서 구조 스타일 내용 등을 변경할 수 있도록 도우며, 구조화된 노드와 오브젝트로 문서를 표현
+
+  
+
+## DOM 조작
+
+- 화면으로 표시된 HTML을 조작어 가능
+
+- **selector** 를 이용해서 조작을 한다 (단일 Node)
+
+  - `querySelector(selector)`를 이용해서 id, class 태그를 선택해서 조작 가능 (id,class,복.선, tag 포함)
+  - `getElementById(id)`는 수업시간에서는 사용하지 않을 예정.
+    - live 속성때문
+
+- HTMLCollection (live)
+
+  - document.getElementsByTagName(tagName)
+  - document.getElementsByClassName(class)
+
+- NodeList(non-live)
+
+  - document.querySelectorAll(selector)
+
+- **Manipulation**
+
+  - innerText 
+
+    - 텍스트
+
+  - innerHTML
+
+    - XSS 공격에 취약점이 있어서 사용시 주의
+
+  - Node attribute
+
+    - element.style.backgroundcolor
+    - setAttribute(attributeName, value)
+    - getAttribute(attributeName)
+
+  - Document.createElement(tagName)
+
+    - 특정 태그를 생성
+
+  - ParentNode.appendChild(Node)
+
+    - 마지막 자식 요소로 추가
+
+  - ParentNode.removeChild(child Node)
+
+    - 해당요소를 제거
+
+    
+
+- 정리
+
+  1. 선택한다
+
+  2. 변경한다
+
+     
+
+
+
+## EventListener
+
+- ### Event
+
+  > click, submit, keydown, mouseover...
+  >
+  > 브라우저에서 일어나는 일
+
+  - **addEventListener**
+
+    - ~하면 ~한다
+
+    - EventTarget. addEventListener( type, listener(function) )
+
+    
--- 알고리즘/baekjun/g_17136(색종이붙이기).py
+++ 알고리즘/baekjun/g_17136(색종이붙이기).py
@@ -1,47 +1,48 @@
 import sys
+# 모든거 탐색하면서 백트래킹 해줘야함
 
 
-def check(x, y, dep):
-    global cnt
+def check(x, y, dep):   # 깊이를 이용해서 .... 백트래킹을 넣어주자
+    global cnt,num
+    if cnt <= dep:
+        return
 
     if matrix[y][x] == 1:
         for k in range(5, 0, -1):
-            if recheck(x, y, k):
-                change(x, y, k, 0)
-                if x + 1 < 10:
+            if 0 < x+k <= 10 and 0 < y+k <= 10 and recheck(x, y, k) and num[k] > 0:
+                # recheck 를 했을 때 통과하면 덮을 수 있음
+                change(x, y, k, 0)  # 0으로 바꿔주고
+                num[k] -= 1 # 종이의 크기 하나 빼준다
+                if x < 9:
                     check(x + 1, y, dep+1)
-                else:
-                    if y + 1 < 10:
+                else:   # x가 9보다 클 때
+                    if y < 9:
                         check((x+1) % 10, y + 1, dep+1)
-                    else:  # 마지막 종료조건인데, 색종이가 잘 붙어있는지 확인해줘야함
-                        print('넘어오나')
-                        for n in range(len(matrix)):
-                            for m in range(len(matrix)):
-                                if matrix[n][m] == 1:
-                                    return
-                        cnt = dep
-                        print(cnt)
+                    else:   # 마지막 종료조건
+                        check(x, y, dep+1)
                 change(x, y, k, 1)
+                num[k] += 1     # 다시 1로 바꾸고 빼줬으면 더해줘야함
 
     else:
-        print(y, x)
-        if x+1 < 10:
+        if x < 9:
             check(x+1, y, dep)
         else:
-            if y+1 < 10:
+            if y < 9:
                 check((x+1) % 10, y+1, dep)
             else:   # 마지막 종료조건인데, 색종이가 잘 붙어있는지 확인해줘야함
                 for n in range(len(matrix)):
                     for m in range(len(matrix)):
                         if matrix[n][m] == 1:
                             return
+                else:
+                    cnt = min(cnt, dep)  # dep 깊이 cnt paper 수
+                    return
 
 
 def recheck(x, y, k):
     for i in range(k):
         for j in range(k):
             if matrix[y+i][x+j] != 1:
-                print('>????')
                 return 0
     return 1
 
@@ -53,9 +54,12 @@ def change(x, y, k, value):
 
 
 matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(10)]
-print(matrix)
 num = [0, 5, 5, 5, 5, 5]
-cnt = 0
+cnt = 26 # 최대 25장 붙일 수 있어서
 check(0, 0, 0)
-print(cnt)
-# 시작점을 찾으면 덮을 공간을 for 문으로 5 4 3 2 1 체크하고 remove하고
+if cnt == 26:
+    print(-1)
+else:
+    print(cnt)
+
+#시작점을 찾으면 덮을 공간을 for 문으로 5 4 3 2 1 체크하고 remove하고
