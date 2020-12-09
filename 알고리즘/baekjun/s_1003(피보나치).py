41855c6104f056840e16617e6b923d2226218015
unknown <dea8307@naver.com>
2020-12-09 10:44:54 +0900

    algo study

--- 알고리즘/baekjun/s_1003(피보나치).py
+++ 알고리즘/baekjun/s_1003(피보나치).py
@@ -1,38 +1,19 @@
 import sys
 
-# def check(n):
-#     global zero, one
-#     if n == 0:
-#         zero += 1
-#         return 0
-#     elif n == 1:
-#         one += 1
-#         return 1
-#     else:
-#         return check(n-1) + check(n-2)
-
 
 def check(n):
-    global zero, one
-    if n in dic:
-        if n == 0:
-            zero += 1
-        elif n == 1:
-            one += 1
-        return dic[n]
-    else:
-        if n == 0:
-            zero += 1
-        elif n == 1:
-            one += 1
-        dic[n] = check(n - 1) + check(n - 2)
-        return dic[n]
+    if n < 2:
+        return
+
+    for i in range(2, n+1):
+        zero.append(zero[i-2]+zero[i-1])
+        one.append(one[i - 2] + one[i - 1])
 
 
 for _ in range(int(sys.stdin.readline())):
-    dic = {0: 0, 1: 1}
-    n =  int(sys.stdin.readline())
-    zero, one = 0, 0
+    zero = [1, 0] # f(3) 일경우는 f1 f2가 포함되어 있음. 그래서 하나씩 더 해준다.
+    one = [0, 1]
+    n = int(sys.stdin.readline())
     check(n)
-    print(zero, end =' ')
-    print(one)
+    print(zero[n], end =' ')
+    print(one[n])
